108962b9ec5493204fecb582e92a12a58bcb46eb
Lewis Danks <lewdanks@gmail.com>
2024-06-01 20:56:04 +0100

    formatting

--- app/game/card-slot.tsx
+++ app/game/card-slot.tsx
@@ -1,3 +1,3 @@
 export default function CardSlot() {
-    return <div className="w-40 h-60 border-2"></div>;
-  }
+  return <div className="w-40 h-60 border-2"></div>;
+}
--- app/game/player-mat.tsx
+++ app/game/player-mat.tsx
@@ -2,25 +2,24 @@ import CardSlot from "./card-slot";
 import CardZone from "./card-zone";
 
 export default function PlayerMat() {
-    return (
+  return (
     <div className="flex p-2 border-4">
       <div id="side-area" className="flex flex-col gap-4">
-        <Deck/>
+        <Deck />
         <Discard />
       </div>
       <div id="play-area" className="flex flex-col gap-4 pl-4">
-        <CardZone cardSlots={8}/>
-        <CardZone cardSlots={9}/>
+        <CardZone cardSlots={8} />
+        <CardZone cardSlots={9} />
       </div>
-  
+
     </div>);
 }
 
 function Deck() {
-    return <CardSlot/>;
-  }
-  
-  function Discard() {
-    return <CardSlot/>;
-  }
-  
+  return <CardSlot />;
+}
+
+function Discard() {
+  return <CardSlot />;
+}
