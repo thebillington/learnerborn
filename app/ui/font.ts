727c3ef99275b081f0d8170113ed266916586b9b
Billy Rebecchi <billy.rebecchi@googlemail.com>
2024-06-01 16:46:16 +0100

    Setup Primary and Secondary fonts

---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const PrimaryFont = Inter({ subsets: ['latin']});
+export const SecondaryFont = Lusitana({ weight: ['400', '700'], subsets: ['latin']});
